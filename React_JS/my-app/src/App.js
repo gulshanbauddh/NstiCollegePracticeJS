4249295cb5b8760de2c4c3eda5bce1d912b375fd
gulshanbauddh <gulshanbncet@gmail.com>
2025-10-06 10:15:52 +0530

    push

--- React_JS/my-app/src/App.js
+++ React_JS/my-app/src/App.js
@@ -1,18 +1,24 @@
-import "./App.css";
-let name = "Gulshan";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <>
-      <h1>Hello {name}</h1>
-      <p>This is my 1st App</p>
-      <p> 
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-        temporibus inventore cum ea unde ab! Harum ad aspernatur illum excepturi
-        vitae odio sapiente eius porro, blanditiis quae dolorem veniam expedita
-        quas error temporibus voluptatem!
-      </p> 
-      <div className="box">I am Gulshan</div>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
