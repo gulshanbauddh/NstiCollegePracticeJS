decfe94444841ef96b87cf35f3656f0bf45a3e78
gulshanbauddh <gulshanbncet@gmail.com>
2025-10-24 13:01:12 +0530

    update

---
+++ Practice/01_Classes_and_object.js
@@ -0,0 +1,13 @@
+const student={
+  name:"Gulshan",
+  fullName:"Gulshan Bauddh",
+  marks:94.6,
+  printMarks : function (){
+    console.log("I am Fuction inside object.");
+  }
+}
+//#1
+// console.log(student.fullName,student.name);
+//#2
+// console.log();
+this.marks;
