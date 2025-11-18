a0a45aba415c74080d55a4ffdfe449a7d2916247
gulshanbauddh <gulshanbncet@gmail.com>
2025-11-18 21:53:52 +0530

    object questions

---
+++ NstiCollegePracticeJs/string.js
@@ -0,0 +1,14 @@
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+}
+
+const myCar1 = new Car("Ford", 2014);
+const myCar2 = new Car("Audi", 2019);
+
+console.log(myCar1);
+const date = new Date();
+console.log(date);
+
