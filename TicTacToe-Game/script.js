f363c3977d09b17951ffb3e99a6c4b7954a34c6f
gulshanbauddh <gulshanbncet@gmail.com>
2025-12-02 00:47:01 +0530

    upddate code

--- TicTacToe-Game/script.js
+++ TicTacToe-Game/script.js
@@ -7,13 +7,13 @@ let msg = document.querySelector("#msg-winner");
 let turnO = true;
 const winPatterns = [
   [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
   [0, 3, 6],
+  [0, 4, 8],
   [1, 4, 7],
   [2, 5, 8],
-  [0, 4, 8],
   [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
 ];
 
 boxs.forEach((box) => {
@@ -21,11 +21,9 @@ boxs.forEach((box) => {
     if (turnO) {
       box.innerText = "0";
       turnO = false;
-      box.disabled = true;
     } else {
       box.innerText = "X";
       turnO = true;
-      box.disabled = true;
     }
     box.disabled = true;
     checkWinner();
