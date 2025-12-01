cd1815ef99edf375f47080fc8921ea47e9ddc214
gulshanbauddh <gulshanbncet@gmail.com>
2025-12-02 01:02:18 +0530

    College work Arrange

---
+++ miniProject/collegeWorkeArrange/data/function.js
@@ -0,0 +1,15 @@
+// #001-
+// function dis(){
+//   console.log("Gulshan Bauddh");
+// }
+
+// #002-
+// let dis=function(a){
+//     console.log("Gulshan Bauddh");
+// }
+
+// #003-
+let dis = () => {
+  console.log("Gulshan Bauddh");
+};
+console.log(dis());
