ed5935a2843c7eee877d844c334f1dc849672c26
gulshanbauddh <gulshanbncet@gmail.com>
2025-12-17 09:25:21 +0530

    deleted unwanted files

--- CodeWithHarryJsHandWritenNotesPractice/destrustring.js
+++
@@ -1,8 +0,0 @@
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// let [a, ...dis] = arr;
-// // console.log(arr);
-// // console.log(a, b, c, a, a, a, a);
-// console.log(dis);
-
-let obj = { a: 1, b: 2, c: 3, d: 4, e: 5 };
-console.log(obj);
