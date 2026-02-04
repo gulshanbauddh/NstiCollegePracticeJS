f91b6ee78c331af2fa16e6461f0e47d148b9ab43
gulshanbauddh <gulshanbncet@gmail.com>
2026-02-04 23:15:37 +0530

    Refactor array initialization and logging in index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-let arr=[5,3];
+let arr=[5,6,8,-5,-78,"Gulshan",'Ravi',7,0]; 
+let arr2;
+arr2=new Array(15);
+console.log(arr);
+console.log(arr2);
+arr2[1]='Arpit';
+console.log(arr2[1]);
+//console.log(arr2[5]);
 
-console.log("Array is : "+arr);
-console.log("Array is arr[1] : "+arr[1]);
-console.log(typeof arr);
-console.log(Array.isArray(arr));
+
+// console.log("Array is : "+arr);
+// console.log("Array is arr[1] : "+arr[1]);
+// console.log(typeof arr);
+// console.log(Array.isArray(arr));
