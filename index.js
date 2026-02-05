f3692e258cdae22ca170cfc54788a22311b870d7
gulshanbauddh <gulshanbncet@gmail.com>
2026-02-05 23:29:05 +0530

    Refactor prime number script for improved clarity and user prompts; clean up index.js

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-let arr=[5,6,8,-5,-78,"Gulshan",'Ravi',7,0]; 
-let arr2;
-arr2=new Array(15);
-console.log(arr);
-console.log(arr2);
-arr2[1]='Arpit';
-console.log(arr2[1]);
-//console.log(arr2[5]);
+// 'use strict';
+// console.log(5 >'a');
+// console.log(0 <'a');
+// console.log(''=='');
+console.log(ReferenceError<=ReferenceError);
 
-
-// console.log("Array is : "+arr);
-// console.log("Array is arr[1] : "+arr[1]);
-// console.log(typeof arr);
-// console.log(Array.isArray(arr));
