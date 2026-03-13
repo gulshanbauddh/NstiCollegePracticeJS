37c2b70604a2371134bebe8b1eaee2a4b5fe9221
gulshanbauddh <gulshanbncet@gmail.com>
2026-03-13 11:37:14 +0530

    asda

--- php/php_Practice_Self/getMethod.php
+++ php/php_Practice_Self/getMethod.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP GET Working</title>
 </head>
+
 <body>
   <h1>PHP GET Working</h1>
   <?php
@@ -15,5 +17,7 @@
     echo "<pre>";
     print_r($_GET);
   ?>
+
 </body>
+
 </html>
