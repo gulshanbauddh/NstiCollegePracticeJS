12032b940132d956e227e1f14cdc1884b732d7ed
gulshanbauddh <gulshanbncet@gmail.com>
2026-03-27 12:37:53 +0530

    oops3

--- php_Practice_Self/oops/02_student.php
+++ php_Practice_Self/oops/02_student.php
@@ -1,17 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>01 Student php</title>
-</head>
-<body>
-  <h2>01 Student php</h2>
-  <?php 
-  class Userr{
-    
+<?php
+class student
+{
+  public string $name, $fName, $mName, $class, $subject;
+  public float $roll, $mark, $fee;
+  public bool $result;
+  // Fee card
+  function feeCard()
+  {
+    echo "
+  \nName:" . $this->name . "
+  \nClass:" . $this->class . "
+  \nRoll no:" . $this->roll . "
+  \nMonthly-Fee:" . $this->fee . "
+  ";
   }
-  
-  ?>
-</body>
-</html>
+  // Scholarship card
+  function scholar()
+  {
+     echo "
+  \nName:" . $this->name . "
+  \nClass:" . $this->class . "
+  \nRoll no:" . $this->roll . "
+  \nMonthly-Fee:" . $this->fee . 
+  "\nScholarShip:" . $this-> fee*12 . "
+  ";
+  }
+  // Marksheet
+  function marksheet(){
+        echo "
+  \nName:" . $this->name . "
+  \nClass:" . $this->class . "
+  \nRoll no:" . $this->roll . "
+  \nTotal-Mark:" . $this->mark . "
+  \nResult:" . $this->result . "
+  ";
+  }
+}
+
+$student1= new student();
