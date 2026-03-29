cf90d332c5944de6b658cc042a30d4a657839a45
gulshanbauddh <gulshanbncet@gmail.com>
2026-03-29 17:26:18 +0530

    oops

--- php_Practice_Self/oops/02_student.php
+++ php_Practice_Self/oops/02_student.php
@@ -1,9 +1,10 @@
 <?php
 class student
 {
-  public string $name, $fName, $mName, $class, $subject;
-  public float $roll, $mark, $fee;
+  public string $name, $fName, $mName, $subject;
+  public float $mark, $fee;
   public bool $result;
+  public int $roll, $class;
   // Fee card
   function feeCard()
   {
@@ -17,17 +18,18 @@ class student
   // Scholarship card
   function scholar()
   {
-     echo "
+    echo "
   \nName:" . $this->name . "
   \nClass:" . $this->class . "
   \nRoll no:" . $this->roll . "
-  \nMonthly-Fee:" . $this->fee . 
-  "\nScholarShip:" . $this-> fee*12 . "
+  \nMonthly-Fee:" . $this->fee .
+      "\nScholarShip:" . $this->fee * 12 . "
   ";
   }
   // Marksheet
-  function marksheet(){
-        echo "
+  function marksheet()
+  {
+    echo "
   \nName:" . $this->name . "
   \nClass:" . $this->class . "
   \nRoll no:" . $this->roll . "
@@ -36,5 +38,20 @@ class student
   ";
   }
 }
+echo "\n";
+$student1 = new student();
 
-$student1= new student();
+$student1->name = 'Gulshan';
+$student1->fName = 'Govind';
+$student1->mName = 'Soniya';
+$student1->subject = 'Science';
+$student1->mark = 97.65;
+$student1->fee = 12364;
+$student1->result = true;
+$student1->roll = 1234;
+$student1->class = 12;
+$student1->feeCard();
+echo "\nScholarship";
+$student1->scholar();
+
+$student2 = new student();
